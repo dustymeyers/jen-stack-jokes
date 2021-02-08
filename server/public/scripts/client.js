f17bc660cd62b44415d591a18b411b57ae7052de
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-08 13:56:08 -0600

    base mode accomplished

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -23,27 +23,36 @@ function addNewJoke() {
     },
     url: '/joke',
     method: 'POST',
-  }).then(function () {
-    console.log('Joke submitted');
-  });
+  })
+    .then(function () {
+      console.log('Joke submitted');
+      fetchJokes();
+    })
+    .catch(function (error) {
+      console.log('There seems to be an error:', error);
+    });
 }
 
 function fetchJokes() {
   $.ajax({
     url: '/jokes',
     method: 'GET',
-  }).then(function (jokeHistory) {
-    console.log('We got some jokes:', jokeHistory);
-    for (let joke of jokeHistory) {
-      $('#outputDiv').append(`
+  })
+    .then(function (jokeHistory) {
+      console.log('We got some jokes:', jokeHistory);
+      for (let joke of jokeHistory) {
+        $('#outputDiv').append(`
           <li>
             <blockquote>
               <p class="jokeQuestionOut">"${joke.jokeQuestion}"</p>
-              <p class="punchLineOut">"${joke.punchLine}"<p>
-              <p class="whoseJokeOut">-- By ${joke.whoseJoke}<p>
+              <p class="punchLineOut">"${joke.punchLine}"</p>
+              <p class="whoseJokeOut">-- By ${joke.whoseJoke}</p>
             </blockquote>
           </li>
       `);
-    }
-  });
+      }
+    })
+    .catch(function (error) {
+      console.log('Could not get jokes', error);
+    });
 }
